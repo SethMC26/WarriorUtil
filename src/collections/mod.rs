8eecb77a4609cb6a22663c0804224344f03de281
Seth Holtzman <holtzmans@merrimack.edu>
2026-03-17 10:59:34 -0400

    feat: Added debug panic macro

--- src/collections/mod.rs
+++ src/collections/mod.rs
@@ -1,1 +1,11 @@
 pub mod skip_list;
+
+/// Triggers a panic in debug builds with the given message.
+/// # Note
+/// Macro simply wraps `debug_assert(false, $msg)``
+#[macro_export]
+macro_rules! debug_panic {
+    ($msg: expr) => {
+        debug_assert!(false, $msg)
+    };
+}
